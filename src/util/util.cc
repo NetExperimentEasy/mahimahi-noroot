663226b04a86b0ea2f60980da00691b8604ff6d8
JaceLau <1446103183@qq.com>
2022-08-10 11:24:11 +0800

    Update util.cc

--- src/util/util.cc
+++ src/util/util.cc
@@ -164,9 +164,9 @@ vector< string > list_directory_contents( const string & dir )
 
 void assert_not_root( void )
 {
-    if ( ( geteuid() == 0 ) or ( getegid() == 0 ) ) {
-        throw runtime_error( "BUG: privileges not dropped in sensitive region" );
-    }
+//     if ( ( geteuid() == 0 ) or ( getegid() == 0 ) ) {
+//         throw runtime_error( "BUG: privileges not dropped in sensitive region" );
+//     }
 }
 
 TemporarilyUnprivileged::TemporarilyUnprivileged()
